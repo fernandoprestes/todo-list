ede169a465846e35c92ce1595214240eb401e0dc
fernando <fernandoprestesc@gmail.com>
2022-06-17 17:26:04 -0300

    Initial commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+<template lang="">
+  <div class="grid h-screen place-items-center">
+    <h1 class="text-2xl font-bold">Hello World</h1>
+    <teste />
+  </div>
+</template>
+
+<style lang=""></style>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx,vue}', './src/**/*', './index.html'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
